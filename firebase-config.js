05f9894abfca04f4735c06781c1d7ed3e5df5564
dumaleja <jared.dumale@afacademy.af.edu>
2026-03-04 10:38:44 -0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -14,13 +14,13 @@
 //     }
 // ============================================================
 const FIREBASE_CONFIG = {
-  apiKey:            "YOUR_API_KEY",
-  authDomain:        "YOUR_PROJECT_ID.firebaseapp.com",
+  apiKey:            "AIzaSyAr5MMHUOtStXD6xkcZorwAkqQ9AfYmg4g",
+  authDomain:        "geo412-games.firebaseapp.com",
   databaseURL:       "https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId:         "YOUR_PROJECT_ID",
-  storageBucket:     "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId:             "YOUR_APP_ID"
+  projectId:         "geo412-games",
+  storageBucket:     "geo412-games.firebasestorage.app",
+  messagingSenderId: "414961619458",
+  appId:             "1:414961619458:web:fe7bbd2764a0e49821cd1b"
 };
 
 firebase.initializeApp(FIREBASE_CONFIG);
